543c190d74676442a9d1aeed835e961d8ce48dbb
bla-bla500 <hermannc125@gmail.com>
2025-10-07 11:11:01 -0400

    5.2 Keeping Score

--- pls don't crash/Assets/Scripts/GameManager.cs
+++ pls don't crash/Assets/Scripts/GameManager.cs
@@ -2,14 +2,21 @@ using NUnit.Framework;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using System.Runtime.CompilerServices;
 
 public class GameManager : MonoBehaviour
 {
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
     public List<GameObject> targets;
     public float spawnRate = 1.0f;
+    public bool gameOver = false;
+
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        scoreText.text = ("Score: " + score);
     }
 
     IEnumerator SpawnTarget()
@@ -17,9 +24,15 @@ public class GameManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(spawnRate);
-            int index = Random.Range(0, targets.Count);
+            int index = UnityEngine.Random.Range(0, targets.Count);
             Instantiate(targets[index]); 
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = ("Score: " + score);
     }
 
     void Update()
--- pls don't crash/Assets/Scripts/Target.cs
+++ pls don't crash/Assets/Scripts/Target.cs
@@ -1,11 +1,18 @@
+
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public int pointValue;
     private Rigidbody thisRB;
     public float spinSpeed = 5;
+    private GameManager gameManagerScript;
+    public ParticleSystem particles;
     void Start()
     {
+
+        gameManagerScript = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
         spinSpeed = Random.Range(-spinSpeed,spinSpeed);
 
         thisRB = GetComponent<Rigidbody>();
@@ -16,11 +23,20 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown() 
     {
+        Instantiate(particles, gameObject.transform.position, gameObject.transform.rotation);
         Destroy(gameObject);
+        gameManagerScript.UpdateScore(pointValue);
+        if (gameObject.CompareTag("Bad"))
+        {
+            gameManagerScript.gameOver = true;
+            gameManagerScript.score = 0;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
     }
+
+    
 }
