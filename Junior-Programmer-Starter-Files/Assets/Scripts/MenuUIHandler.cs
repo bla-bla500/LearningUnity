7cfaae2e3755bb2455b43cf3080bd4760f689c42
bla-bla500 <hermannc125@gmail.com>
2025-10-20 10:54:08 -0400

    Implement Data Persistence Between Sessions

--- Junior-Programmer-Starter-Files/Assets/Scripts/MenuUIHandler.cs
+++ Junior-Programmer-Starter-Files/Assets/Scripts/MenuUIHandler.cs
@@ -24,17 +24,33 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
     public void StartNewScene(int whichScene)
     {
         SceneManager.LoadScene(whichScene);
     }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
     public void ExitApp()
     {
+        MainManager.Instance.SaveColor();
 #if UNITY_EDITOR
     EditorApplication.ExitPlaymode();
 #else 
     Application.Quit();
 #endif
     }
+
+
 }
